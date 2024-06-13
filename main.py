cfb5bf82fdc9ac7b6aa10e5f96332aee71259aa3
AyushGupta2114 <ayushgupta220503@gmail.com>
2024-06-13 18:07:08 +0530

    data updating in sql ( user table )

--- main.py
+++ main.py
@@ -24,5 +24,17 @@ class DBHelper:
         cur.execute(query)
         print("Created")
 
+    def insert_user(self,userid,username,phone):
+        query="insert into user(userId,userName,phone) values({},'{}','{}')".format(userid,username,phone)
+        print(query)
+        cur=self.con.cursor()
+        cur.execute(query)
+        self.con.commit()
+        print('User saved to db')
+
 #main code
 helper=DBHelper()
+helper.insert_user(1325,"Pooja","2225")
+helper.insert_user(323,"Mohit","2125")
+helper.insert_user(334,"Aman","2545")
+helper.insert_user(534,"Piyush","2735")
